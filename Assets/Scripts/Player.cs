a8b5269fcff4238d5f0437b72075173d488ef33c
Andrew Mitchell <ajmitchell79@hotmail.com>
2020-06-26 21:58:34 +0100

    part  4

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public float hitPoints = 100f;
     private Rigidbody2D rb;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if the player pressed space (exclude holding key down)
+            GameObject go = Instantiate(bulletPrefab, gameObject.transform);
+            Bullet bullet = go.GetComponent<Bullet>();
+            bullet.targetVector = new Vector3(1, 1, 0);
+        }
     }
 
     void FixedUpdate()
